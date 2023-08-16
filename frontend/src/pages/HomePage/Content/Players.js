b122aab868c9c008546b4da0c5384e1021e20202
gavuolo <gabs.vuolo@gmail.com>
2023-08-16 19:12:55 -0400

    Fix: homepage/player style
    Signed-off-by: gavuolo <gabs.vuolo@gmail.com>

--- frontend/src/pages/HomePage/Content/Players.js
+++ frontend/src/pages/HomePage/Content/Players.js
@@ -1,18 +1,22 @@
 import styled from "styled-components";
 import { NavInput } from "../../../components/Input/NavInput";
 import { ContentBox } from "./styles";
+import { IoSearchSharp } from "react-icons/io5";
 
 export default function Players() {
   return (
     <>
-      <ContentBox bgcolor='#9B9999'>
+      <ContentBox bgcolor="#9B9999">
         <RoleDescription>
           <h1>JOGADORES</h1>
           <div></div>
         </RoleDescription>
         <SearchBox>
-          <NavInput placeholder={"Nome do jogaddor"} />
-          <IconSearch></IconSearch>
+          <NavInput placeholder={"Nome do jogaddor"} width={"100%"} />
+          <IconSearch>
+            <div></div>
+            <IoSearchSharp />
+          </IconSearch>
         </SearchBox>
       </ContentBox>
     </>
@@ -30,6 +34,26 @@ const RoleDescription = styled.div`
     background-color: #f99e1a;
   }
 `;
-const IconSearch = styled.div``;
+const IconSearch = styled.div`
+  position: absolute;
+  right: 0;
+  left: 1520px;
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  color: black;
+  cursor: pointer;
+  div:nth-child(1) {
+    width:  3px;
+    height: 20px;
+    background-color: #EEEEEE;
+    border-radius: 50px;
+    margin: 0 1px 0 0;
+  }
+`;
 const SearchBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 63%;
 `;
